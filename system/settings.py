a5a1ee2597cab8c9a58b3b48dada6fe5ccf9fa59
lucasmarrtt <lucasschnneider009@gmail.com>
2023-05-07 13:18:52 -0300

    mudando host

--- system/settings.py
+++ system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q(c$g^vj2ngwl+-g^=n$mg12t-o)r98nwr5w^-o7s#mezk!7r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", 'https://labcortcu.com/']
 
 
 # Application definition
